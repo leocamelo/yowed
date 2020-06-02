35865a36beb74558f11d20b73c5c82418b33de7e
leocamelo <leonardocamelo.nave@gmail.com>
2020-06-01 23:54:47 -0300

    remove nprogress

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,16 +4,11 @@ import 'phoenix_html';
 
 import { Socket } from 'phoenix';
 import { LiveSocket } from 'phoenix_live_view';
-// import NProgress from 'nprogress';
 
 const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 const liveSocket = new LiveSocket('/live', Socket, { params: { _csrf_token: csrfToken } });
 
 liveSocket.connect();
-
-// NProgress.configure({ showSpinner: false });
-// window.addEventListener('phx:page-loading-start', () => NProgress.start());
-// window.addEventListener('phx:page-loading-stop', () => NProgress.done());
 
 const applyStyle = (id, css) => {
   let $style = document.getElementById(id);
